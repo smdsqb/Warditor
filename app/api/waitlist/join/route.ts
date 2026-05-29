1d453cf33e7e7c8b9c0f9f4918313c9959c38d17
Saqib <backupsaqib@proton.me>
2026-05-29 11:15:45 +0800

    Update route.ts

--- app/api/waitlist/join/route.ts
+++ app/api/waitlist/join/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { db } from '../../../../lib/firebase-admin'
+import { db } from '@/lib/firebase-admin'
 import { FieldValue } from 'firebase-admin/firestore'
 import { Resend } from 'resend'
 
